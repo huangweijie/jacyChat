f9434826c841803dd929c9edf5db4471697076bd
jacyhuang <jacy.huang@vipshop.com>
2017-07-10 16:03:25 +0800

    project build

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <appHeader></appHeader>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script src="../static/js/frozen.js"></script>
+<script>
+  import 'frozenui'
+  import appHeader from '@/components/header'
+  export default {
+    name: 'app',
+    components: {
+      appHeader,
+    },
+    created() {
+      //初始化rem
+      function resize() {
+        var windowWidth = window.innerWidth;
+        document.documentElement.style.fontSize = windowWidth/10 + 'px';
+      }
+      resize();
+      window.onresize = resize;
+    }
+  }
+</script>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,28 @@
+<template>
+	<header>
+     	<span>Jacy Chat</span>
+     	<div class="ui-icon-refresh refresh"></div>
+    </header>
+</template>
+
+<style>
+	header {
+		position: relative;
+		width: 100%;
+		height: 1rem;
+		background-color: #57c2e8;
+		font-size: 0.5rem;
+		line-height: 1rem;
+		color: #fff;
+		padding-left: 0.2rem;
+		overflow: hidden;
+		box-sizing: border-box;
+  	}
+	.refresh {
+		position: absolute;
+		top: 0;
+		right: 2rem;
+		font-size: 1rem;
+		line-height: 1rem;
+	}
+</style>
